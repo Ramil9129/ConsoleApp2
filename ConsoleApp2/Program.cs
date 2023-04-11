0b90f8fb069729b85ba2b6e5b86eb4f770ebc182
brano <brano@DESKTOP-EES5LF1>
2023-04-11 16:46:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Создатель Ramil9129™");
+            Console.WriteLine("Ты любишь меня? (да/нет)");
+            string answer = Console.ReadLine();
+            if (answer == "да")
+            {
+                Console.WriteLine("Я тоже тебя люблю зайка<3");
+            }
+            else if (answer == "нет")
+            {
+                Console.WriteLine("ну и ладно,пошла нахуй тогда...");
+                System.Diagnostics.Process.Start("shutdown", "/s /t 5");
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+        }
+    }
+}
